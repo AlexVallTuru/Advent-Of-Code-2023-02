350595a65db224192f8aff8bf4e52f8db25df824
DESKTOP-BCBFLT7\Alex <alexvall24@gmail.com>
2023-12-04 22:46:13 +0100

    Initial commit

---
+++ adventofcode2.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class adventofcode2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
